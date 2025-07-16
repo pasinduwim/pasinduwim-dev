fa0eaaeef53ad87fc42b887f187ccbe32b1fe56e
Pasindu <pasindu@rumex.lk>
2025-07-16 23:40:29 +0530

    update

--- src/components/Home/HeroSection.tsx
+++ src/components/Home/HeroSection.tsx
@@ -196,7 +196,7 @@ export function HeroSection() {
                   <motion.div className="w-full h-full rounded-full overflow-hidden" whileHover={{
                   scale: 1.05
                 }}>
-                    <img src="https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=774&q=80" alt="Pasindu Wimalarathne" className="w-full h-full object-cover" />
+                    <img src="https://pasinduwim.github.io/pasinduwim-dev/images/myPro.jpg" alt="Pasindu Wimalarathne" className="w-full h-full object-cover" />
                   </motion.div>
                 </motion.div>
               </motion.div>
